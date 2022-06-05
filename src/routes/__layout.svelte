bdce6dc98fb880ca204b6d6439d74352370f2099
ali <alin02912@gmail.com>
2022-06-05 00:02:24 -0400

    some minor updates

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,20 @@
+<script>
+import "../styles/global.css"
+</script>
+<header>
+<div class="logo">AN</div>
+<div class="hamburger">Hamburger</div>
+</header>
+<main>
+
+</main>
+
+<style>
+    header {
+        display: flex;
+        justify-content: space-between;
+    }
+    header > * {
+        margin: 1rem;
+    }
+</style>
